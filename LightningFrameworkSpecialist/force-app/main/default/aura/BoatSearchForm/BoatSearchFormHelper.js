83ed1655aa89352e5891ef4878910205aadfc629
CansuAsci <cansu.asci@osf-global.com>
2019-02-28 15:24:39 +0300

    Completed until Challenge 5

--- LightningFrameworkSpecialist/force-app/main/default/aura/BoatSearchForm/BoatSearchFormHelper.js
+++ LightningFrameworkSpecialist/force-app/main/default/aura/BoatSearchForm/BoatSearchFormHelper.js
@@ -1,22 +1,34 @@
 ({
-    loadBoatTypes: function(component){
-    //create the action
-            console.log("Helper started");
-            var action = component.get("c.getBoatTypes");
+    getBoatTypeNames : function(component) {
+        //Set Action Name on Aura Controller and its parameters
+        var actionName = 'getBoatTypeNames';
+        var params = {};
 
-            //add the callback behavior for when the response is received
-            action.setCallback(this,function(response){
-            var state = response.getState();
-            if (state === "SUCCESS"){
-                component.set("v.BoatTypes", response.getReturnValue());
-                console.log(response.getReturnValue());
-                }
-                else {
-                console.log("Failed with state: " + state);
-                }
-            });
+        //Call N3Service apexRequest method, define response handler, and catch handler
+        this.apexRequest(component, actionName, params)
+            .then(this.handleGetBoatTypeNamesResponse(component))
+            .catch(this.handleError(component));
 
-            //send action off to be executed
-            $A.enqueueAction(action);
-       },
+	},
+    handleGetBoatTypeNamesResponse : function (component) {
+        return $A.getCallback(function (response) {
+            console.log(response)
+            //Set Boat Type Names in aura attribute
+            var boatTypes = response;
+            component.set('v.boatTypes', boatTypes)
+            console.log(component.get('v.boatTypes'))
+        }.bind(this));
+    },
+    handleError : function (component) {
+        return $A.getCallback(function (error) {
+            //Log error and display toast
+            console.log(error);
+            var opts = {
+                title : 'Boat Search Form',
+                message : error,
+                type : 'error'
+            }
+            this.displayToast(opts)
+        }.bind(this));
+    }
 })
--- LightningFrameworkSpecialist/force-app/main/default/aura/BoatSearchResults/BoatSearchResultsHelper.js
+++ LightningFrameworkSpecialist/force-app/main/default/aura/BoatSearchResults/BoatSearchResultsHelper.js
@@ -1,5 +1,36 @@
 ({
-    helperMethod : function() {
+   onSearch : function(component,event, helper){
+        var boatTypeId = component.get('v.boatTypeId');
+        var action = component.get('c.getBoats');
+        console.log('boatTypeId:', boatTypeId)
+        action.setParams({
+            boatTypeId : boatTypeId
+        });
+        action.setCallback(this, function(response) {
+            console.log(response)
+            if(response.getState() === 'SUCCESS') {
+                console.log(response.getReturnValue())
 
+                //Set list of boats in boats attribute
+                component.set('v.boats', response.getReturnValue());
+                component.set('v.ready', true);
+            } else {
+                //If error, use handleError method
+                this.handleError(component);
+            }
+        });
+        $A.enqueueAction(action);
+   },
+   handleError : function (component) {
+        return $A.getCallback(function (error) {
+            //Log error and display toast
+            console.log(error);
+            var opts = {
+                title : 'Boat Search Result',
+                message : error,
+                type : 'error'
+            };
+            this.displayToast(opts)
+        }.bind(this));
     }
 })
